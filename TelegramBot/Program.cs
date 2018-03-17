4616f76e81c937f6437cfe79dae352807cfc23f0
Ященко Сергей <dammit@live.ru>
2018-03-17 22:43:49 +0300

    добавил обновление новостей каждый час по таймеру

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using TelegramBot.ParserCore;
 using TelegramBot.ParserCore.Habra;
@@ -25,12 +26,22 @@ namespace TelegramBot
             parser.OnCompleted += ParseCommand.Parser_OnCompleted;
             parser.OnNewData += ParseCommand.Parser_OnNewData;
             parser.Settings = new HabraSettings(1, 1);  // первая страница сайта
-            parser.Start();
+            //parser.Start();   //при работе с таймером эту строчку закомментируем
+            // устанавливаем метод обратного вызова
+            TimerCallback tcb = new TimerCallback(Get2chNews);
+            // создаем таймер
+            Timer timer = new Timer(tcb, parser, 0, 3600000);   //будем получать новости каждый час
             #endregion
 
             bot.RunAsync().Wait();
-
             Console.ReadKey();
+        }
+        //получаем новые новости
+        static void Get2chNews(object obj)
+        {
+            ParserWorker<string[]> parser = (ParserWorker<string[]>)obj;
+            parser.Start();
+            //Console.WriteLine("Текущее время:  " + DateTime.Now.ToLongTimeString());
         }
     }
 }

17eebd5009fdd4a9b19861fcdf53dd3a950b9475
Ященко Сергей <dammit@live.ru>
2018-03-21 12:05:30 +0300

    stavklass

--- TelegramBot/ParserCore/Habra/HabraParser.cs
+++ TelegramBot/ParserCore/Habra/HabraParser.cs
@@ -15,12 +15,15 @@ namespace TelegramBot.ParserCore.Habra
             //var items = document.QuerySelectorAll("a").Where(item => item.ClassName != null && item.ClassName.Contains("post__title_link"));  //habr
             //var items = document.QuerySelectorAll("div").Where(item => item.ClassName != null && item.ClassName.Contains("text"));    //nekdo
             //var items = document.QuerySelectorAll("a").Where(item => item.ClassName != null && item.ClassName.Contains("orange")).OfType<IHtmlAnchorElement>(); //2ch
-            var items = document.QuerySelectorAll("a").Where(item => item.ClassName != null && item.ClassName.Contains("b-list-item__link")).OfType<IHtmlAnchorElement>(); //lenta.ru
+            //var items = document.QuerySelectorAll("a").Where(item => item.ClassName != null && item.ClassName.Contains("b-list-item__link")).OfType<IHtmlAnchorElement>(); //lenta.ru
+            //var items = document.QuerySelectorAll("a").Where(item => item.ClassName != null && item.ClassName.Contains("story__title-link")).OfType<IHtmlAnchorElement>(); //pikabu
+            var items = document.All.Where(item => item.LocalName != null && item.LocalName.Contains("img")).OfType<IHtmlImageElement>(); //stavklass
             foreach (var item in items)
             {
-                //list.Add(item.TextContent);	//habr, nekdo
-                //list.Add("https://2ch.hk" + item.PathName);	//2ch
-                list.Add("https://m.lenta.ru" + item.PathName);	//lenta.ru
+                list.Add(item.Source); //habr, nekdo, stavklass
+                //list.Add("https://2ch.hk" + item.PathName);   //2ch
+                //list.Add("https://m.lenta.ru" + item.PathName);	//lenta.ru
+                //list.Add("https://pikabu.ru" + item.PathName);	//pikabu
             }
             return list.ToArray();
         }
--- TelegramBot/ParserCore/Habra/HabraSettings.cs
+++ TelegramBot/ParserCore/Habra/HabraSettings.cs
@@ -10,10 +10,11 @@
         //public string BaseUrl { get; set; } = "https://habrahabr.ru";
         //public string BaseUrl { get; set; } = "https://nekdo.ru";
         //public string BaseUrl { get; set; } = "https://2ch.hk/news/";
-        public string BaseUrl { get; set; } = "https://m.lenta.ru/";
+        //public string BaseUrl { get; set; } = "https://m.lenta.ru/";
+        public string BaseUrl { get; set; } = "http://stavklass.ru/images";
         //public string Prefix { get; set; } = "page{CurrentId}";   //habr
         //public string Prefix { get; set; } = "page/{CurrentId}";  //nekdo
-        public string Prefix { get; set; } = "";    //2ch, lenta.ru
+        public string Prefix { get; set; } = "";    //2ch, lenta.ru, pikabu, stavklass
         public int StartPoint { get; set; }
         public int EndPoint { get; set; }
     }

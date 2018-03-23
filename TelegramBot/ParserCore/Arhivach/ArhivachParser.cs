7542a4631367421ac4c6dcb1f4a46b61d723de8e
Ященко Сергей <dammit@live.ru>
2018-03-23 14:42:06 +0300

    1

--- TelegramBot/ParserCore/Arhivach/ArhivachParser.cs
+++ TelegramBot/ParserCore/Arhivach/ArhivachParser.cs
@@ -12,14 +12,15 @@ namespace TelegramBot.ParserCore.Arhivach
         public string[] Parse(IHtmlDocument document)
         {
             var list = new List<string>();
-           
+
             //var items = document.QuerySelectorAll("a").Where(item => item.ClassName != null && item.ClassName.Contains("post__title_link"));  //habr
             //var items = document.QuerySelectorAll("div").Where(item => item.ClassName != null && item.ClassName.Contains("text"));    //nekdo
             //var items = document.QuerySelectorAll("a").Where(item => item.ClassName != null && item.ClassName.Contains("orange")).OfType<IHtmlAnchorElement>(); //2ch
             //var items = document.QuerySelectorAll("a").Where(item => item.ClassName != null && item.ClassName.Contains("b-list-item__link")).OfType<IHtmlAnchorElement>(); //lenta.ru
             //var items = document.QuerySelectorAll("a").Where(item => item.ClassName != null && item.ClassName.Contains("story__title-link")).OfType<IHtmlAnchorElement>(); //pikabu
             //var items = document.All.Where(item => item.LocalName != null && item.LocalName.Contains("img")).OfType<IHtmlImageElement>(); //stavklass
-            var items = document.QuerySelectorAll("a")/*.Where(item => item.Style == null && item.ClassName.Contains("thread_text"))*/.OfType<IHtmlAnchorElement>(); //arhivach
+            //var items = document.QuerySelectorAll("a")/*.Where(item => item.Style == null && item.ClassName.Contains("thread_text"))*/.OfType<IHtmlAnchorElement>(); //arhivach
+            var items = document.QuerySelectorAll("a").Where(item => item.Attributes["style"] != null).Select(x => x.Attributes["href"].Value); //arhivach
             foreach (var item in items)
             {
                 //list.Add(item.TextContent); //habr, nekdo
@@ -27,9 +28,9 @@ namespace TelegramBot.ParserCore.Arhivach
                 //list.Add("https://2ch.hk" + item.PathName);   //2ch
                 //list.Add("https://m.lenta.ru" + item.PathName);	//lenta.ru
                 //list.Add("https://pikabu.ru" + item.PathName);	//pikabu
-                if (item.PathName.Contains("/thread/"))
+                if (item.Contains("/thread/"))
                 {
-                    list.Add("http://arhivach.org" + item.PathName);    //arhivach
+                    list.Add("http://arhivach.org" + item);    //arhivach
                 }
             }
             return list.ToArray();
@@ -37,3 +38,5 @@ namespace TelegramBot.ParserCore.Arhivach
     }
 }
 
+//AngleSharp example
+//var links= document.QuerySelectorAll("a.object-title-a.text-truncate").Where(item => item.Attributes["href"]!=null).Select(x=>x.item.Atributtes["href"].Value).ToList();

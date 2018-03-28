ef6506157eb0e0b8b07251b359996038f3f10fe8
Ященко Сергей <dammit@live.ru>
2018-03-28 13:41:56 +0300

    1

--- TelegramBot/ParserCore/Pen/PenParser.cs
+++ TelegramBot/ParserCore/Pen/PenParser.cs
@@ -20,19 +20,19 @@ namespace TelegramBot.ParserCore.Pen
             //var items = document.All.Where(item => item.LocalName != null && item.LocalName.Contains("img")).OfType<IHtmlImageElement>(); //stavklass, pornpics
             //var items = document.QuerySelectorAll("div").Where(item => item.ClassName == "thumb_container").Select(x => x.Attributes["data-previewvideo"].Value); //xhamster
             //var items = document.QuerySelectorAll("a").Where(item => item.Attributes["href"] != null && item.Attributes["href"].Value.Contains("/wall-")).Select(x => x.Attributes["href"].Value.Remove(0, 1)); //vk (Remove(0, 1) - удалим первый символ "/")
-            var items = document.QuerySelectorAll("a").Where(item => item.Attributes["href"] != null && item.Attributes["href"].Value.Contains("http://penitto.ru/?p=")).OfType<IHtmlAnchorElement>(); //penitto.ru
+            var items = document.QuerySelectorAll("a").Where(item => item.Attributes["href"] != null && item.Attributes["rel"] != null && item.Attributes["href"].Value.Contains("http://penitto.ru/?p=")).Select(x => x.Attributes["href"].Value); //penitto.ru
             foreach (var item in items)
             {
                 //if (item.Source.Length < 100)
                 //{
                 //    list.Add(item.Source); //stavklass, pornpics
                 //}
-                //list.Add(item); //xhamster
+                list.Add(item); //xhamster, penitto.ru
                 //list.Add(item.TextContent); //habr, nekdo
                 //list.Add("https://2ch.hk" + item.PathName);   //2ch
                 //list.Add("https://m.lenta.ru" + item.PathName);	//lenta.ru
                 //list.Add("https://pikabu.ru" + item.PathName);	//pikabu
-                list.Add("https://vk.com/tip32?w=" + item); //vk
+                //list.Add("https://vk.com/tip32?w=" + item); //vk
             }
             return list.ToArray();
         }
